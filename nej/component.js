5e8e96df5beb318cfc4128080b6c174900418f07
JOVI <jovi_lin@outlook.com>
2019-07-04 10:45:07 +0800

    Update component.js

--- nej/component.js
+++ nej/component.js
@@ -3,7 +3,7 @@
  *
  * @version  1.0
  * @author  {{author}} {{email}} {{date}}
- * @module   
+ * @module  {{name[0].toUpperCase() + name.slice(1)}}
  */
 NEJ.define([
     'pool/component-base/src/base'

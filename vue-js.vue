8f9a09ac548e01202f7259ee3fe388560f9a8094
jovi <linjuewei@corp.netease.com>
2019-11-15 16:58:48 +0800

    vue

---
+++ vue-js.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="m-{{name}}">
+  </div>
+</template>
+<script>
+export default {
+  name:"{{name}}",
+  data() {
+    return {};
+  },
+  computed:{
+    foo:{
+      get(){
+        return 
+      }
+    }
+  },
+  methods: {
+    
+  }
+};
+</script>
+<style lang="scss">
+
+</style>
